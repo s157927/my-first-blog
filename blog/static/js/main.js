b1873e2a61707f7d9eec7d37834e00c9eb40be42
s157927 <a.dagkilic@student.tue.nl>
2017-12-16 20:09:12 +0100

    PhotoLand original view integrated

---
+++ blog/static/js/main.js
@@ -0,0 +1,75 @@
+﻿$(document).ready(() => {
+  $('.login-button').on('click', () => {// Clicking login opens login form and hides signup form
+    $('.signup-form').hide();
+    $('.login-form').toggle();
+    //$('.login-button').toggleClass('button-active-login');
+    $('html,body').scrollTop(0);
+
+  });
+
+  $('.signup-button').on('click', () => {//Clicking sing up opens sign up forn and hides log in form
+    $('.login-form').hide();
+    $('.signup-form').toggle();
+    //$('.signup-button').toggleClass('button-active-signup');
+    $('html,body').scrollTop(0);
+  });
+
+  $('#getstarted').on('click', () => {//Clicking Get Started opens sign up forn and hides log in form
+    $('.login-form').hide();
+    $('.signup-form').toggle();
+    $('html,body').scrollTop(0);
+  });
+  $('#getstarted2').on('click', () => {//Clicking Get Started opens sign up forn and hides log in form
+    $('.login-form').hide();
+    $('.signup-form').toggle();
+    $('html,body').scrollTop(0);
+  });
+
+  $(".login-button").click(function(event) {//On small devices, clicking login or signup will collapse the navbar
+    if (!$(this).parent().hasClass('dropdown'))
+        $(".navbar-collapse").collapse('hide');
+  })
+  $(".signup-button").click(function(event) {
+    if (!$(this).parent().hasClass('dropdown'))
+        $(".navbar-collapse").collapse('hide');
+  })
+
+
+    $('.card-btn1').on('click', event => {//LIKE button click funtion
+      $(event.currentTarget).toggleClass('active');
+    });
+
+    $(".card-btn1").click(function() {
+    if ($(this).text() == "Like") {
+        $(this).text("Unlike");
+    } else {
+        $(this).text("Like");
+    };
+
+});
+
+
+
+
+
+
+
+
+
+
+/*
+  $('.card-btn1').on('click', () => {
+    if ($(".card-btn1").is(":disabled"))
+    {
+      $('.card-btn1').prop('disabled', false);
+    }
+    else if ($(".card-btn1").is(":not(:disabled)"))
+    {
+      $('.card-btn1').prop('disabled', true);
+    }
+  })
+
+
+*/
+
+});
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.index, name='index'),
+    url(r'^examples/$', views.examples, name='examples')
 ]
